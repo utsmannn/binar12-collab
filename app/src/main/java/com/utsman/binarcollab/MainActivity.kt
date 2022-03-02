20d4ebd9e5b9cd8b09674012c13087849e4f0a89
mjafarshidik <mjafarshidik86@gmail.com>
2022-03-02 19:49:15 +0700

    Update Main Activity

--- app/src/main/java/com/utsman/binarcollab/MainActivity.kt
+++ app/src/main/java/com/utsman/binarcollab/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var testing = "Testing"
-    
+    var testing = "Test"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
